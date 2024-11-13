51c8cc139ca7214879467b0c0196cab510f6bc0c
ramiowo <yerami1@naver.com>
2024-11-13 17:49:51 +0900

    base

---
+++ src/Router.js
@@ -0,0 +1,13 @@
+// import { HashRouter, Route, Routes } from "react-router-dom";
+
+// const Router = () => {
+//   return (
+//     <HashRouter>
+//       <Routes>
+//         <Route></Route>
+//       </Routes>
+//     </HashRouter>
+//   );
+// };
+
+// export default Router;
